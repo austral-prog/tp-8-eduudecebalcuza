fe5111ea8ae45e383c6c3aba80aa170cc8b59dad
eduudecebalcuza <162722197+eduudecebalcuza@users.noreply.github.com>
2024-05-24 12:10:32 -0300

    Update sets.py

--- sets.py
+++ sets.py
@@ -2,7 +2,10 @@ from sets_categories_data import ALCOHOLS
 
 
 def clean_ingredients(dish_name, dish_ingredients):
-    my_set = set(dish_ingredients)
+    my_set = set()
+    for element in dish_ingredients:
+        if element not in my_set:
+            my_set.add(element)
     return (dish_name, my_set)
 
 def check_drinks(drink_name, drink_ingredients):
